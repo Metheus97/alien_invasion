4223c7e4eccd8ec5357049231b99661ec6498e64
Matheus Pereira <78231377+Metheus97@users.noreply.github.com>
2021-09-28 13:04:03 -0300

    Initial commit

---
+++ game_functions.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+
+
+def check_keydown_events(event, ship):
+    """Responde a pressionamento de tecla."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Responde a Soltura de tecla."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ship):
+    """Responde a eventos de pressionamento de teclas e de mouse."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screen(ai_settings, screen, ship):
+    """Atualiza as imagens na tela a alterna para a nova tela."""
+    # Redesenha a tela a cada passagem pelo laço
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Deixar a tela mais recente visivel
+    pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,13 @@
+
+class Settings():
+    '''Uma classe para armazenar tadas as configurações da Invasão Alienígena.'''
+
+    def __init__(self):
+        """Incializando as configurações do jogo."""
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 680
+        self.bg_color = (255, 255, 255)
+
+        # Configuração da espaçonave
+        self.ship_speed_factor = 1.5
